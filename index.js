3f97aeb37ffc7dd873c538abf7e288e970331ee3
Ralph Nicholson <nicholson.re@gmail.com>
2023-05-12 12:35:06 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,90 @@
 /* Your Code Here */
 
+class Employee {
+    constructor(arr) {
+        this.firstName = arr[0];
+        this.familyName = arr[1];
+        this.title = arr[2];
+        this.payPerHour = arr[3];
+        this.timeInEvents = [];
+        this.timeOutEvents = [];
+    }
+
+    allWagesFor() {
+        const eligibleDates = this.timeInEvents.map(function (e) {
+            return e.date
+        })
+
+        const payable = eligibleDates.reduce(function (memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d)
+        }.bind(this), 0)
+        return payable
+    }
+}
+
+function createEmployeeRecord(arr) {
+    const employee = new Employee(arr);
+    employee.hoursWorkedOnDate = hoursWorkedOnDate.bind(employee);
+    return employee;
+}
+
+function createEmployeeRecords(nestedArrays) {
+    return nestedArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    const parsedHour = parseInt(hour, 10);
+
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: parsedHour,
+        date: date,
+    }
+    this.timeInEvents.push(timeInEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    const parsedHour = parseInt(hour, 10);
+
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: parsedHour,
+        date: date,
+    }
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = this.hoursWorkedOnDate(date);
+    const payOwed = hoursWorked * this.payPerHour;
+    return payOwed;
+}
+
+function findEmployeeByFirstName(collection, firstName) {
+    return collection.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    let totalPayroll = 0;
+    for (const employeeRecord of employeeRecords) {
+        const employeeWages = employeeRecord.allWagesFor();
+        totalPayroll += employeeWages;
+    }
+    return totalPayroll;
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
